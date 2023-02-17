6d6921b508c31dfeb3f89e48cc30c903b3563bdb
tasnimalam001 <tasnimalam.001@gmail.com>
2023-02-17 12:28:54 +0600

    try left added and some update

--- js/z.js
+++ js/z.js
@@ -1,4 +1,16 @@
 document.getElementById('generate-pin').addEventListener('click',function(){
+    const success = document.getElementById('pin-success');
+    const failure = document.getElementById('pin-failure');
+    failure.style.display='none';
+    success.style.display='none';
+
+
+    const typedPin = document.getElementById('typed-numbers');
+    typedPin.value = '';
+
+
+
+
     const pin = getPin();
     const pinField = document.getElementById('display-pin');
     pinField.value = pin;    
@@ -25,21 +37,40 @@ document.getElementById('calculator').addEventListener('click',function(event){
 })
 
 document.getElementById('verify-pin').addEventListener('click',function(){
-    const genaratePin = getStringFromInput('display-pin');
-    const typedPin = getStringFromInput('typed-numbers');
-    console.log(genaratePin,typedPin);
-    const success = document.getElementById('pin-success');
-    const failure = document.getElementById('pin-failure');
-    if(genaratePin === typedPin){       
-        success.style.display='block';
-        failure.style.display ='none';
-        const valueField = document.getElementById('typed-numbers');
-        valueField.value ='';
-        const pinField = document.getElementById('display-pin');
-        pinField.value = ''; 
+    const tryleft = document.getElementById('try-left');
+    const tryleftString = tryleft.innerText;
+    const tryleftNumber = parseFloat(tryleftString);
+
+    if(tryleftNumber > 0){
+        const genaratePin = getStringFromInput('display-pin');
+        const typedPin = getStringFromInput('typed-numbers');
+        console.log(genaratePin,typedPin);
+        const success = document.getElementById('pin-success');
+        const failure = document.getElementById('pin-failure');
+        if(genaratePin === typedPin){       
+            success.style.display='block';
+            failure.style.display ='none';
+            const valueField = document.getElementById('typed-numbers');
+            valueField.value ='';
+            const pinField = document.getElementById('display-pin');
+            pinField.value = ''; 
+        }
+        else{
+            failure.style.display='block';
+            success.style.display='none';
+            const tryleftNumberAfter = tryleftNumber - 1;
+            tryleft.innerText = tryleftNumberAfter;
+        }
+   
+    
     }
     else{
-        failure.style.display='block';
-        success.style.display='none';
+        
+        alert('No more try left');
+        
+
     }
+    
+    
+
 })
